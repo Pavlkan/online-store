4fd708e90f4d5ba76993444e575531dc9d63c71b
Svetlana <lanavladi1605gmail.com>
2022-12-17 16:13:12 +0500

    fix: add new instance footerController & append it in root src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { FooterController } from './controller/FooterController';
 import { HeaderController } from "./controller/HeaderController";
 import "./styles.css";
 
@@ -7,5 +8,9 @@ root.id = "root";
 const headerController = new HeaderController();
 
 root.append(headerController.component.element);
+
+const footerController = new FooterController();
+
+root.append(footerController.component.element);
 
 document.body.append(root);
