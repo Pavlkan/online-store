a6e8a131c7cd5882f6af63f97f60f93f5158718c
Svetlana <lanavladi1605gmail.com>
2023-01-05 14:29:11 +0500

    fix: add router src/controller/HeaderController.ts

--- src/controller/HeaderController.ts
+++ src/controller/HeaderController.ts
@@ -1,11 +1,12 @@
-import { HeaderComponent } from "../view/components/header/HeaderComponent";
-import { BaseController } from "./BaseController";
+import { HeaderComponent } from '../view/components/header/HeaderComponent';
+import { Router } from '../view/Router';
+import { BaseController } from './BaseController';
 
 export class HeaderController extends BaseController<HeaderComponent> {
-    public component: HeaderComponent;
+  public component: HeaderComponent;
 
-    constructor() {
-        super();
-        this.component = new HeaderComponent(this);
-    }
+  constructor(router: Router) {
+    super();
+    this.component = new HeaderComponent(this, router);
+  }
 }
